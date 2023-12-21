b55ffd182d0e871293ac5d9e493419ee45fca492
sadik312 <sadikislam46@gmail.com>
2023-12-21 22:53:03 +0000

    created controller

---
+++ MvcMovie/Controllers/HelloWordController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController: Controller
+{
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action";
+    }
+
+    // GET: /HelloWorld/Welcome/
+    public string Welcome(string name, int numTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is: {numTimes}");
+    }
+}
